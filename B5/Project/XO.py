441eb523e5dd9ac9d812bbc6dd22f24242d6c2ef
OneHandedPirate <plagueismkii@gmail.com>
2022-10-05 17:52:10 +1000

    Add

--- B5/Project/XO.py
+++ B5/Project/XO.py
@@ -1,8 +1,7 @@
 import os
-import time
 
 print('Добро пожаловать в игру Крестики-Нолики!\n\n!!!Внимание!!!\n'
-      'За попытку занять уже занятую клетку последует кара!')
+      'За попытку занять уже занятую клетку последует кара!\n')
 
 
 def win_cond(f, t):
@@ -13,26 +12,27 @@ def win_cond(f, t):
                 f[2][0] == f[1][1] == f[0][2] == t[-2]:
             os.system('cls')
             print(f'Победили {t[-2].upper()}!')
-            return False
+            return True
     if not '-' in f[0] and not '-' in f[1] and not '-' in f[2]:
         os.system('cls')
         print(f'Победила дружба!')
-        return False
-    else:
         return True
+    else:
+        return False
 
 
 def show_f(field):
-    print(f'\n  0 1 2')
+    print(f'\n \033[33m 0 1 2\033[0m')
     for ind, val in enumerate(field):
-        print(f'{ind} {" ".join(val)}')
+        print(f'\033[33m{ind}\033[0m {" ".join(val)}')
 
 
 def game():
     pf = [['-'] * 3 for _ in range(3)]
     turns = ['o', 'x'] * 6
 
-    while win_cond(pf, turns):
+    while not win_cond(pf, turns):   #Пока условия выигрыша не соблюдаются игра продолжается.
+        print(f'Ход {13 - len(turns)}, ходит игрок {turns[-1].upper()}')
         show_f(pf)
         current_fig = turns[-1]
         trn = input(f'Введите координаты следующего {current_fig.upper()} через пробел ('
@@ -44,11 +44,11 @@ def game():
                 turns.pop()
             else:
                 os.system('cls')
-                print(f'Хорошая попытка, но нет. Вы дисквалифицированы! Победил {turns[-2].upper()}\n')
+                print(f'Хорошая попытка, но нет. Вы дисквалифицированы!'
+                      f' Победил игрок {turns[-2].upper()}\n')
                 break
-        except:  # Обрабатываем все возможные варианты неверного ввода за раз
-            print('Неверный формат ввода, попробуйте еще раз через 3 секунды.\n')
-            time.sleep(3)
+        except:  # Обрабатываем все возможные варианты неверного ввода за раз (числа в не диапазона, некорректный ввод и т.д.)
+            print('Неверный формат ввода, попробуйте еще раз\n')
 
     q = input('Хотите сыграть еще раз? (Y/N) ')
     if q.lower() == 'y':
